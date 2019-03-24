c21f42ed679c13bec07cc9b4d0d29a6d7fb8d8cd
graffingCalculator <47076741+graffingCalculator@users.noreply.github.com>
2019-03-24 18:31:15 +0200

    Codecademy generated from the project submission page.

---
+++ jammming/src/components/App/App.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
+import Spotify from '../../util/Spotify';
+
+class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      searchResults: [],
+      playlistName:'New Playlist',
+      playlistTracks: []
+  };
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
+  }
+
+  addTrack(track) {
+    if (this.state.playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
+    }
+    let tracks = this.state.playlistTracks;
+    tracks.push(track);
+    this.setState({playlistTracks: tracks});
+
+  }
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+    this.setState({playlistTracks: tracks});
+  }
+
+  updatePlaylistName(name) {
+    this.setState({playlistName: name});
+  }
+
+  savePlaylist() {
+    const trackUris = this.state.playlistTracks.map(track => track.uri);
+    let name = this.state.playlistName;
+    Spotify.savePlaylist(name, trackUris)
+    if(this.state.playlistTracks.length) {
+      this.setState({
+          playlistName: 'New Playlist',
+          playlistTracks: []
+        });
+      }
+  }
+
+  search(term) {
+    Spotify.search(term).then(searchResults => {
+      this.setState({searchResults: searchResults});
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+        <SearchBar onSearch={this.search}/>
+        <div className="App-playlist">
+        <SearchResults  searchResults={this.state.searchResults}
+                        onAdd={this.addTrack}/>
+        <Playlist playlistTracks={this.state.playlistTracks}
+                  onRemove={this.removeTrack}
+                  onNameChange={this.updatePlaylistName}
+                  onSave={this.savePlaylist} />
+        </div>
+      </div>
+    </div>
+    );
+  }
+}
+
+export default App;
